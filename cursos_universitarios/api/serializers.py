b6cc92d26ebac246b4dea91e971aa70ca25a0cea
Ramiro Nocelli <rjnocelli@Ramiros-MacBook-Pro.local>
2021-01-03 14:19:56 -0300

    Crear AlumnoSerializador serializador

--- cursos_universitarios/api/serializers.py
+++ cursos_universitarios/api/serializers.py
@@ -1,2 +1,11 @@
+from cursos.models import Alumno, Curso, Suscripcion
+from rest_framework import serializers
 
+class AlumnoSerializador(serializers.ModelSerializer):
+    promedio = serializers.DecimalField(source='obtener_promedio_total', max_digits=4, decimal_places=2)
+    cantidad_de_cursos = serializers.IntegerField(source='obtener_cantidad_total_de_cursos')
+    cantidad_de_cursos_aprobados = serializers.IntegerField(source='obtener_cantidad_cursos_aprobados')
 
+    class Meta:
+        model = Alumno
+        fields = '__all__'

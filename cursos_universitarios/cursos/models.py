06b312a5022578a16d5dd0605f2c405eb7c76f1f
Ramiro Nocelli <rjnocelli@Ramiros-MacBook-Pro.local>
2021-01-03 14:01:19 -0300

    Crear clase Suscripcion

--- cursos_universitarios/cursos/models.py
+++ cursos_universitarios/cursos/models.py
@@ -15,3 +15,12 @@ class Curso(models.Model):
 
     def __str__(self):
         return f'{self.nombre}'
+
+class Suscripcion(models.Model):
+    curso = models.ForeignKey("Curso", verbose_name="Curso", on_delete=models.CASCADE)
+    alumno = models.ForeignKey("Alumno", verbose_name="Alumno", on_delete=models.CASCADE)
+    calificacion = models.DecimalField(null=True, blank=True, max_digits=4, decimal_places=2)
+    fecha_de_inicio = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.curso.nombre} - {self.alumno.nombre} {self.alumno.apellido}'

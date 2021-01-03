0a9e908d19c32b3339d257056f9173fbb0672f9d
Ramiro Nocelli <rjnocelli@Ramiros-MacBook-Pro.local>
2021-01-03 13:58:35 -0300

    Crear clase Alumno

--- cursos_universitarios/cursos/models.py
+++ cursos_universitarios/cursos/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Alumno(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254)
+    dni = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.nombre} {self.apellido}'

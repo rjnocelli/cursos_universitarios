453fcdf63ab898c15fd06b9a35f82bce44ebb967
Ramiro Nocelli <rjnocelli@Ramiros-MacBook-Pro.local>
2021-01-04 12:41:26 -0300

    Agregar urls para CursoViewSet

--- cursos_universitarios/api/urls.py
+++ cursos_universitarios/api/urls.py
@@ -4,8 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.api_vistas, name='api'),
     path('alumnos/', views.AlumnoViewSet.as_view(), name='alumnos-lista'),
-    path('alumnos/<alumno_id>/', views.AlumnoViewSet.as_view(), name='alumnos'),
-    path('curso/<curso_id>/', views.obtener_curso, name='cursos-lista'),
+    path('alumnos/<alumno_id>/', views.AlumnoViewSet.as_view(), name='alumno'),
+    path('cursos/', views.CursoViewSet.as_view(), name='cursos-lista'),
+    path('cursos/<curso_id>/', views.CursoViewSet.as_view(), name='curso'),
     path('suscripcion/', views.SuscripcionViewSet.as_view(), name='suscripcion'),
     path('curso/<curso_id>/imprimir-lista-alumnos/', views.imprimir_listado_alumnos, name='imprimir-listado-alumnos'),
 

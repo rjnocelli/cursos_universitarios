ab22f3e31714eed235f0662b2bd476b152c54f6b
Ramiro Nocelli <rjnocelli@Ramiros-MacBook-Pro.local>
2021-01-04 12:37:56 -0300

    Crear vista CursoViewSet

--- cursos_universitarios/api/views.py
+++ cursos_universitarios/api/views.py
@@ -5,33 +5,22 @@ from rest_framework.response import Response
 from rest_framework import generics, viewsets, status
 from rest_framework.views import APIView
 from cursos.models import Alumno, Curso, Suscripcion
-from .serializers import (ServidorClienteAlumnoSerializador, ClienteServidorAlumnoSerializador, CursoSerializador,
-                         ServidorClienteSuscripcionSerializador, ClienteServidorSuscripcionSerializador,
+from .serializers import (ServidorClienteAlumnoSerializador, ClienteServidorAlumnoSerializador, ServidorClienteCursoSerializador,
+                         ClienteServidorCursoSerializador, ServidorClienteSuscripcionSerializador, ClienteServidorSuscripcionSerializador,
                          ClienteServidorAgregarCalificacionSerializador)
                         
 
 @api_view(['GET'])
 def api_vistas(request):
     api_urls = {
-        'almunos-lista': '/alumnos-lista/',
-        'cursos-lista': '/cursos-lista/',
-        'curso-detalles': '/curso/<curso_id>/',
+        'almunos-lista': '/alumnos/',
+        'alumnos-id': '/alumnos/<alumno_id>/',
+        'cursos-lista': '/cursos/',
+        'cursos-id': '/cursos/<curso_id>/',
         'curso-suscriptores-csv': 'curso/<curso_id>/imprimir-lista-alumnos/',
         'suscripcion (CRUD)': '/suscripcion/',
     }
     return Response(api_urls)
-
-@api_view(['GET'])
-def obtener_lista_cursos(request):
-    cursos = Curso.objects.all()
-    serializer = CursoSerializador(cursos, many=True)
-    return Response(serializer.data)
-
-@api_view(['GET'])
-def obtener_curso(request, curso_id):
-    curso = Curso.objects.get(id = curso_id)
-    serializer = CursoSerializador(curso)
-    return Response(serializer.data)
 
 class AlumnoViewSet(APIView):
 
@@ -61,6 +50,33 @@ class AlumnoViewSet(APIView):
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+class CursoViewSet(APIView):
+
+    def get(self, request, **kwargs):
+        if kwargs:
+            query = Curso.objects.get(id=kwargs['curso_id'])
+            serializer = ServidorClienteCursoSerializador(query)
+        else:
+            query = Curso.objects.all()
+            serializer = ServidorClienteCursoSerializador(query, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = ClienteServidorCursoSerializador(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def put(self, request, **kwargs):
+        curso = Curso.objects.get(id=kwargs['curso_id'])
+        serializer = ClienteServidorCursoSerializador(curso, data = request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class SuscripcionViewSet(APIView):
     

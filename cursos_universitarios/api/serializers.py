d828a6bdecb485642d35f6a338775c7ee78d72bb
Ramiro Nocelli <rjnocelli@Ramiros-MacBook-Pro.local>
2021-01-04 12:20:45 -0300

    Crear ClienteServidorAlumnosSerializadorClienteServidor serializador

--- cursos_universitarios/api/serializers.py
+++ cursos_universitarios/api/serializers.py
@@ -1,7 +1,9 @@
 from cursos.models import Alumno, Curso, Suscripcion
 from rest_framework import serializers
 
-class AlumnoSerializador(serializers.ModelSerializer):
+# Serializadores para clase Alumno
+
+class ServidorClienteAlumnoSerializador(serializers.ModelSerializer):
     promedio = serializers.DecimalField(source='obtener_promedio_total', max_digits=4, decimal_places=2)
     cantidad_de_cursos = serializers.IntegerField(source='obtener_cantidad_total_de_cursos')
     cantidad_de_cursos_aprobados = serializers.IntegerField(source='obtener_cantidad_cursos_aprobados')
@@ -9,6 +11,14 @@ class AlumnoSerializador(serializers.ModelSerializer):
     class Meta:
         model = Alumno
         fields = '__all__'
+
+class ClienteServidorAlumnosSerializadorClienteServidor(serializers.ModelSerializer):
+
+    class Meta:
+        model = Alumno
+        fields = '__all__'
+
+# Serializadores para clase Curso
 
 class CursoSerializador(serializers.ModelSerializer):
     alumnos_suscriptos = serializers.IntegerField(source='obtener_cantidad_alumnos_suscriptos')

6a86f27b373a1f07926205b3ad25197e001a5b3d
Ramiro Nocelli <rjnocelli@Ramiros-MacBook-Pro.local>
2021-01-04 10:16:28 -0300

    Crear vista index en aplicacion cursos

--- cursos_universitarios/cursos_universitarios/urls.py
+++ cursos_universitarios/cursos_universitarios/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from cursos import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    path('', views.index)
 ]

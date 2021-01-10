e59024281af9dd983b85d9898c22a3847b05b0cf
Ramiro Nocelli <rjnocelli@Ramiros-MacBook-Pro.local>
2021-01-10 11:01:57 -0300

    Modificar app.module.ts - agregar modulos necesarios - agregar routing

--- frontend/cursos-front/src/app/app.module.ts
+++ frontend/cursos-front/src/app/app.module.ts
@@ -1,7 +1,10 @@
+import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { HttpClientModule } from '@angular/common/http'
+import { RouterModule } from '@angular/router';
 
-import { AppComponent } from './app.component';
 import { AccionesDisponiblesComponenteComponent } from './acciones-disponibles-componente/acciones-disponibles-componente.component';
 import { AgregarAlumnoComponenteComponent } from './agregar-alumno-componente/agregar-alumno-componente.component';
 import { AgregarCursoComponenteComponent } from './agregar-curso-componente/agregar-curso-componente.component';
@@ -28,9 +31,24 @@ import { CalificarAlumnoComponenteComponent } from './calificar-alumno-component
     CalificarAlumnoComponenteComponent
   ],
   imports: [
-    BrowserModule
-  ],
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: '', component: AccionesDisponiblesComponenteComponent },
+      { path: 'cursos', component: CursosListaComponenteComponent },
+      { path: 'cursos/:cursoId', component: CursoDetallesComponenteComponent },
+      { path: 'alumnos/:alumnoId', component: AlumnoDetallesComponenteComponent },
+      { path: 'alumnos', component: AlumnosListaComponenteComponent },
+      { path: 'agregar-alumno', component: AgregarAlumnoComponenteComponent },
+      { path: 'agregar-curso', component: AgregarCursoComponenteComponent, },
+      { path: 'suscribir', component: SuscripcionComponenteComponent },
+      { path: 'desuscribir', component: DesuscripcionComponenteComponent },
+      { path: 'calificar', component: CalificarAlumnoComponenteComponent },
+    ])
+  ], 
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }

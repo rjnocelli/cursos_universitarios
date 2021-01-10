7de99635f55ac37bf5cd2c659634004bdaaa8179
Ramiro Nocelli <rjnocelli@Ramiros-MacBook-Pro.local>
2021-01-10 11:13:28 -0300

    Modificar agregar-alumno-componente - crear form para agregar alumno a la base de datos

--- frontend/cursos-front/src/app/agregar-alumno-componente/agregar-alumno-componente.component.ts
+++ frontend/cursos-front/src/app/agregar-alumno-componente/agregar-alumno-componente.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { ApiServiceService } from '../api.service';
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-agregar-alumno-componente',
@@ -6,8 +9,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./agregar-alumno-componente.component.css']
 })
 export class AgregarAlumnoComponenteComponent implements OnInit {
+  agregarAlumnoForm;
+  alumno_data;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder, 
+              private api: ApiServiceService,   
+              private router: Router) { 
+                this.agregarAlumnoForm
+
+    this.agregarAlumnoForm = this.formBuilder.group({
+      nombre: '',
+      apellido: '',
+      email: '',
+      dni: '',
+    });
+  }
+
+  enviarAlumnoData(alumnoData) {
+    this.api.agregarAlumno(alumnoData).subscribe(
+      data => {
+        this.alumno_data = data
+        alert(`alumno agregado a la base de datos`)
+      },
+      error => {
+        alert('ERROR!!!')
+        console.log(error)
+      }
+    )
+    this.agregarAlumnoForm.reset();
+    this.router.navigate(['alumnos'])
+  }
 
   ngOnInit(): void {
   }

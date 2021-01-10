b59523c7bb95f2dfad6d0a7d78f94f0777dc7795
Ramiro Nocelli <rjnocelli@Ramiros-MacBook-Pro.local>
2021-01-10 11:28:34 -0300

    Modificar curso-detalles-componente

--- frontend/cursos-front/src/app/curso-detalles-componente/curso-detalles-componente.component.ts
+++ frontend/cursos-front/src/app/curso-detalles-componente/curso-detalles-componente.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ApiServiceService } from '../api.service';
 
 @Component({
   selector: 'app-curso-detalles-componente',
   templateUrl: './curso-detalles-componente.component.html',
   styleUrls: ['./curso-detalles-componente.component.css']
 })
-export class CursoDetallesComponenteComponent implements OnInit {
+export class CursoDetallesComponenteComponent {
+  curso;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private route: ActivatedRoute,
+              private api: ApiServiceService) { 
+      
+      this.obtenerCursoDetalles() 
+              
+    }
+  
+    obtenerCursoDetalles = () =>{
+    const cursoId = this.route.snapshot.params.cursoId;
+    this.api.obtenerCurso(cursoId).subscribe(
+      data => {
+        this.curso = data
+      },
+      error => {
+        console.log(error)
+      }
+    )
   }
-
 }

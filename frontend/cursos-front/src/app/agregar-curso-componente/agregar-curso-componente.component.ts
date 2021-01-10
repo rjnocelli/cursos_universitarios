c57896f66e9e987b6ebbd4609f520979b2144ed2
Ramiro Nocelli <rjnocelli@Ramiros-MacBook-Pro.local>
2021-01-10 11:17:22 -0300

    Modificar agregar-curso-componente - crear form para agregar alumnos a la db

--- frontend/cursos-front/src/app/agregar-curso-componente/agregar-curso-componente.component.ts
+++ frontend/cursos-front/src/app/agregar-curso-componente/agregar-curso-componente.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { ApiServiceService } from '../api.service';
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-agregar-curso-componente',
   templateUrl: './agregar-curso-componente.component.html',
   styleUrls: ['./agregar-curso-componente.component.css']
 })
-export class AgregarCursoComponenteComponent implements OnInit {
+export class AgregarCursoComponenteComponent{
+  agregarCursoForm;
+  curso_data;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder,
+              private api: ApiServiceService, 
+              private router: Router) { 
 
-  ngOnInit(): void {
+    this.agregarCursoForm = this.formBuilder.group({
+      nombre: '',
+      descripcion: '',
+    });          
+  }
+
+  enviarCursoData(cursoData) {
+    this.api.agregarCurso(cursoData).subscribe(
+      data => {
+        this.curso_data = data
+        alert(`curso agregado a la base de datos`)
+      },
+      error => {
+        alert('ERROR!!!')
+        console.log(error)
+      }
+    )
+    this.agregarCursoForm.reset();
+    this.router.navigate(['cursos/'])
   }
 
 }

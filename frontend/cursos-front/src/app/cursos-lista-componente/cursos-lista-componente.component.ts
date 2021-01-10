db57f9755810c8e66a77b600fc0937c47f6dd10c
Ramiro Nocelli <rjnocelli@Ramiros-MacBook-Pro.local>
2021-01-10 11:30:33 -0300

    Modificar cursos-lista-componente

--- frontend/cursos-front/src/app/cursos-lista-componente/cursos-lista-componente.component.ts
+++ frontend/cursos-front/src/app/cursos-lista-componente/cursos-lista-componente.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { ApiServiceService } from '../api.service';
 
 @Component({
   selector: 'app-cursos-lista-componente',
   templateUrl: './cursos-lista-componente.component.html',
   styleUrls: ['./cursos-lista-componente.component.css']
 })
-export class CursosListaComponenteComponent implements OnInit {
+export class CursosListaComponenteComponent {
+  cursos;
 
-  constructor() { }
+  constructor(private api: ApiServiceService) { }
 
-  ngOnInit(): void {
+  obtenerCursos = () => {
+    this.api.obtenerCursosTodos().subscribe(
+      data => {
+        this.cursos = data
+      },
+      error => {
+        console.log(error)
+      }
+    )
+  }
+
+  borrarCurso = (curso_id) => {
+    this.api.eliminarCurso(curso_id).subscribe(
+      data => {
+        this.cursos = this.cursos.filter(item => item.id != curso_id)
+        alert("curso borrado de base de datos")
+      },
+      error => {
+        console.log(error)
+      });
   }
 
 }

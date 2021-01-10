1c393fff8091b9483266854f891b0c3d8cdee71e
Ramiro Nocelli <rjnocelli@Ramiros-MacBook-Pro.local>
2021-01-10 10:12:09 -0300

    Installar django-cors-headers

--- cursos_universitarios/cursos_universitarios/settings.py
+++ cursos_universitarios/cursos_universitarios/settings.py
@@ -41,11 +41,16 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cursos',
     'rest_framework',
+    'corsheaders',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',

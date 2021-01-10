1d0743fa2cd412ce100f680cfe08a3287f862736
Ramiro Nocelli <rjnocelli@Ramiros-MacBook-Pro.local>
2021-01-10 11:25:51 -0300

    Modificar alumnos-lista-componente

--- frontend/cursos-front/src/app/alumnos-lista-componente/alumnos-lista-componente.component.ts
+++ frontend/cursos-front/src/app/alumnos-lista-componente/alumnos-lista-componente.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiServiceService } from '../api.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-alumnos-lista-componente',
   templateUrl: './alumnos-lista-componente.component.html',
   styleUrls: ['./alumnos-lista-componente.component.css']
 })
-export class AlumnosListaComponenteComponent implements OnInit {
+export class AlumnosListaComponenteComponent {
+  alumnos;
+  alumno_id;
 
-  constructor() { }
+  constructor(private api: ApiServiceService,
+              private router: Router,) {
+                
+              this.obtenerAlumnos();
+      }
+  
+  obtenerAlumnos = () => {
+    this.api.obtenerAlumnosTodos().subscribe(
+      data => {
+        this.alumnos = data
+      },
+      error => {
+        console.log(error)
+      })
+    }
 
-  ngOnInit(): void {
+  borrarAlumno = (alumno_id) => {
+    this.api.eliminarAlumno(alumno_id).subscribe(
+      data => {
+        this.alumnos = this.alumnos.filter(item => item.id != alumno_id)
+        alert("alumno borrado de base de datos")
+      },
+      error => {
+        console.log(error)
+      })
   }
+
+  
 
 }

a1b673fd617361529098a6e092afd74b728e4785
Ramiro Nocelli <rjnocelli@Ramiros-MacBook-Pro.local>
2021-01-10 11:45:53 -0300

    Modificar calificar-alumno-componente

--- frontend/cursos-front/src/app/calificar-alumno-componente/calificar-alumno-componente.component.ts
+++ frontend/cursos-front/src/app/calificar-alumno-componente/calificar-alumno-componente.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiServiceService } from '../api.service';
+import { FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-calificar-alumno-componente',
@@ -6,10 +9,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calificar-alumno-componente.component.css']
 })
 export class CalificarAlumnoComponenteComponent implements OnInit {
+  alumnos;
+  cursos;
+  calificacion;
+  calificarAlumnoForm;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder,
+              private api: ApiServiceService,
+              private router: Router) {
+    this.calificarAlumnoForm = this.formBuilder.group({
+      alumno: '',
+      curso: '',
+      calificacion: 0,
+    });
   }
+  ngOnInit(): void {
+    this.api.obtenerAlumnosTodos().subscribe(
+      data => {
+        this.alumnos = data
+      }, 
+      error => {
+        console.log(error)
+      });
+    this.api.obtenerCursosTodos().subscribe(
+      data => {
+        this.cursos = data
+      }, 
+      error => {
+        console.log(error)
+      }
+    )}
 
+  calificarAlumno(data){
+    this.api.calificarAlumno(data).subscribe(
+      data => {
+        console.log(data)
+        alert(`calificaion agregada`)
+      },
+      error => {
+        console.log(error)
+        alert(`ERROR!!! Prueba suscribir al alumno primero!`)
+      })
+    this.calificarAlumnoForm.reset();
+    this.router.navigate([''])
+  }
 }

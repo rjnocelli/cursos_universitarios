e258a5829649afb2cd59f32af9991367988f9af4
Ramiro Nocelli <rjnocelli@Ramiros-MacBook-Pro.local>
2021-01-10 11:39:05 -0300

    Modificar desuscripcion-componente

--- frontend/cursos-front/src/app/desuscripcion-componente/desuscripcion-componente.component.ts
+++ frontend/cursos-front/src/app/desuscripcion-componente/desuscripcion-componente.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { ApiServiceService } from '../api.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-desuscripcion-componente',
@@ -6,10 +9,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./desuscripcion-componente.component.css']
 })
 export class DesuscripcionComponenteComponent implements OnInit {
+  desuscribirAlumnoForm;
+  alumnos;
+  cursos;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder,
+              private api: ApiServiceService,
+              private router: Router) {
+    
+    this.desuscribirAlumnoForm = this.formBuilder.group({
+      alumno: '',
+      curso: '',
+    });
+  }         
 
   ngOnInit(): void {
+    this.api.obtenerAlumnosTodos().subscribe(
+      data => {
+        this.alumnos = data
+      }, 
+      error => {
+        console.log(error)
+      }
+    );
+    this.api.obtenerCursosTodos().subscribe(
+      data => {
+        this.cursos = data
+      }, 
+      error => {
+        console.log(error)
+      }
+    )
+  }
+
+  desuscribirAlumno(data){
+    this.api.desuscribirAlumno(data).subscribe(
+      data => {
+        console.log(data)
+        alert(`alumno desuscripto`)
+      },
+      error => {
+        alert('ERROR!!! Alumno no suscripto a este curso!')
+        console.log(error)
+      })
+    this.desuscribirAlumnoForm.reset();
+    this.router.navigate([''])
   }
 
 }

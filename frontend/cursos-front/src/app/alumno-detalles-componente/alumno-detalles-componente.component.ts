4ca9af84495a59cd662c4835f8444d94a66c0979
Ramiro Nocelli <rjnocelli@Ramiros-MacBook-Pro.local>
2021-01-10 11:22:21 -0300

    Modificar alumno-detalles-componente

--- frontend/cursos-front/src/app/alumno-detalles-componente/alumno-detalles-componente.component.ts
+++ frontend/cursos-front/src/app/alumno-detalles-componente/alumno-detalles-componente.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ApiServiceService } from '../api.service';
 
 @Component({
   selector: 'app-alumno-detalles-componente',
   templateUrl: './alumno-detalles-componente.component.html',
   styleUrls: ['./alumno-detalles-componente.component.css']
 })
-export class AlumnoDetallesComponenteComponent implements OnInit {
+export class AlumnoDetallesComponenteComponent{
+  alumno;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,
+               private api: ApiServiceService) { 
+      this.obtenerAlumnoDetalles()
+    }
 
-  ngOnInit(): void {
-  }
-
+  obtenerAlumnoDetalles = () =>{
+    const alumnoId = this.route.snapshot.params.alumnoId;
+      this.api.obtenerAlumno(alumnoId).subscribe(
+        data => {
+          this.alumno = data
+        },
+        error => {
+          console.log(error)
+        }
+      )
+    }
 }
